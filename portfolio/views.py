f641ad176cc4a90890522344011a74c502bfc39b
luis12007 <00129020@edu.sv>
2025-01-28 11:28:26 -0600

    :tada: Creating foundations

---
+++ portfolio/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, HttpResponse
+from .models import TodoItem
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def todos_list(request):
+    todos = TodoItem.objects.all()
+    return render(request, 'todos.html', {'todos': todos})
