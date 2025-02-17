cabebc56c95e56e9eb06d33b55546063b99577d8
luis12007 <00129020@edu.sv>
2025-02-16 23:14:16 -0600

    changing views and adding information

---
+++ static/js/scripts.js
@@ -0,0 +1,26 @@
+function openLinkedIn() {
+    window.open('https://www.linkedin.com/in/alexmtzai/', '_blank');
+}
+
+function openGitHub() {
+    window.open('https://github.com/luis12007', '_blank');
+}
+
+function openEmail() {
+    window.open('mailto:alexmtzai2002@gmail.com', '_blank');
+}
+
+openVideo = function() {
+    window.location.href = '/presentation';
+}
+
+downloadCV = function() {
+    window.open('/static/files/CV.pdf', '_blank');
+}
+opendte = function() {
+    window.open('https://cliente-production-e629.up.railway.app/demostration', '_blank');
+}
+
+openlinkedinbot = function() {
+    window.open('https://github.com/luis12007/Networking-bot', '_blank');
+}
